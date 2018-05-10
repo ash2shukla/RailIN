7774e3a708cd29b9671af0f2334998af46209970
Ashish Shukla <ash2shukla@gmail.com>
2018-05-10 18:18:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 setup(
   name = 'RailIN',
-  packages = ['RailIN'], # this must be the same as the name above
+  packages = ['RailIN'],
   version = '0.2',
   description = 'Unofficial API for Indian Railways.',
   license='MIT',
   author = 'Ashish Shukla',
   author_email = 'ash2shukla@gmail.com',
-  url = 'https://github.com/ash2shukla/RailIN', # use the URL to the github repo
-  download_url = 'https://github.com/ash2shukla/RailIN/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['Railways', 'API', 'IR','unofficial'], # arbitrary keywords
+  url = 'https://github.com/ash2shukla/RailIN',
+  download_url = 'https://github.com/ash2shukla/RailIN/archive/0.1.tar.gz',
+  keywords = ['Railways', 'API', 'IR','unofficial'],
   classifiers = [],
   install_requires=[
           'bs4',
